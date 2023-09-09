1fc958d0a1b3a55f12cbff6e9fdf786ab1ff01c4
Yunosuke Sato <yunosukesato04@gmail.com>
2023-09-09 15:16:02 -0700

    Main section

---
+++ src/app/components/Main.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+function Main() {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET'S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#5651e5]">Yuno</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Web Developer</h1>
+          <p className="py-4 text-gray-600 mx-w-[70%] m-auto">
+            I'm a web developer specializing in building exceptional digital
+            experience. Currently, I'm focused on building responsive front-end
+            web applications while learning algorithms and data structures.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedin />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsPersonLinesFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
