5bfed64f0b7365211e27f9b844a950bc94e9268c
Yunosuke Sato <yunosukesato04@gmail.com>
2023-09-09 15:15:41 -0700

    added Main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Navbar from './components/Navbar'
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <Navbar />
-  )
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
 }
