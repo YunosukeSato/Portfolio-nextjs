42d9fb828b641dc6028be91a8e9a22df7fdd018f
Yunosuke Sato <yunosukesato04@gmail.com>
2023-09-16 21:03:33 -0700

    Project section

---
+++ src/app/components/Projects/projectData.ts
@@ -0,0 +1,31 @@
+import Translator from "../../../../public/assets/projects/Translator.png"
+import EldenRing from "../../../../public/assets/projects/EldenRing.png"
+import TeddyEC from "../../../../public/assets/projects/TeddyEC.png"
+import DailyBlog from "../../../../public/assets/projects/DailyBlog.png"
+
+export const projects = [
+  {
+    title: "ChatGPT Translator",
+    backgroundImg: Translator,
+    projectUrl: "https://react-translator-five.vercel.app/",
+    tech: "React TS ChatGPT"
+  },
+  {
+    title: "Elden Ring LP",
+    backgroundImg: EldenRing,
+    projectUrl: "https://elden-ring-blond.vercel.app/",
+    tech: "HTML SCSS JS"
+  },
+  {
+    title: "Teddy EC",
+    backgroundImg: TeddyEC,
+    projectUrl: "/",
+    tech: "Next TS Strapi"
+  },
+  {
+    title: "Daily Blog",
+    backgroundImg: DailyBlog,
+    projectUrl: "/",
+    tech: "React Node JS MongoDB"
+  },
+]
