53adf315b5d27ffe3ce2a3f7b91f6a0ac46cf0a8
Yunosuke Sato <yunosukesato04@gmail.com>
2023-09-25 17:37:58 -0700

    added url

--- src/app/daily_blog/page.tsx
+++ src/app/daily_blog/page.tsx
@@ -5,10 +5,9 @@ const projectInfo = {
   img: blogImg,
   name: "Daily Blog",
   technologies: ["React JS", "Node", "MongoDB", "Tailwind"],
-  description: `*Currently working to deploy*
-  Blog page using React.js and tailwind for client-side and Node.js and MongoDB for server-side and database. This application allows users to write their own blog about the existing categories. Through the development of this application, I gained hands-on experience in connecting the client-side to a database using Node.js. Additionally, I honed my skills with MongoDB and deepened my understanding of React Router Dom.
+  description: `Blog page using React.js and tailwind for client-side and Node.js and MongoDB for server-side and database. This application allows users to write their own blog about the existing categories. Through the development of this application, I gained hands-on experience in connecting the client-side to a database using Node.js. Additionally, I honed my skills with MongoDB and deepened my understanding of React Router Dom.
   `,
-  url: "/#projects",
+  url: "https://daily-blog-client.vercel.app/",
 };
 
 function page() {
